7519281b33d3701d20529075c273b858144006c9
daoohoangg <daohoang2911@gmail.com>
2025-07-29 16:27:28 +0700

    hoang-portfolio

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="p-6 bg-blue-500 text-white text-xl font-bold rounded-lg">
+    Hello Tailwind + Vue!
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
